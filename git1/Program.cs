c88bbc37eb406eedf3bb1603af34fe4904bd3f95
Frank Nordahl Andersen <fran2238@edu.eal.dk>
2018-09-26 11:08:06 +0200

    ændringer på pc 2

--- git1/Program.cs
+++ git1/Program.cs
@@ -11,7 +11,7 @@ namespace git1
         static void Main(string[] args)
         {
             Console.WriteLine("hello git1 ændret på pc2");
-            Console.WriteLine("git1 er ændret");
+            Console.WriteLine("git1 er ændret af pc 2");
             Console.ReadLine();
 
         }
